bf9becf58c3df1d0821a83695da53b1fafd96797
dkumylove <dkumylove@gmail.com>
2023-10-26 17:39:10 +0900

    테스트 첫번째 커밋

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,18 @@
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요.");
+		System.out.println("Hello.");
+		System.out.println("Hi.");
+		System.out.println("만나서 반갑습니다..");
+		System.out.println("Welcome.");
+		System.out.println("------------------");
+		System.out.print("안");
+		System.out.print("녕");
+		System.out.print("하");
+		System.out.print("세");
+		System.out.print("요");
+		
+	}
+	
+}
